fad8b3fd9afa906538e5ee3beb3cfdbfa48796b2
JuanSolisA <juan.solis.trabajo@gmail.com>
2022-09-23 00:17:21 -0300

    Learning props in react

---
+++ src/components/ContentRowMovies.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const ContentRowMovies = (props) => {
+  return (
+    <div className="col-md-4 mb-4">
+      <div className={props.color}>
+        <div className="card-body">
+          <div className="row no-gutters align-items-center">
+            <div className="col mr-2">
+              <div className={props.estiloTitulo}>
+                {props.titulo}
+                </div>
+              <div className="h5 mb-0 font-weight-bold text-gray-800">
+                {props.cifra}
+              </div>
+            </div>
+            <div className="col-auto">
+              <i className={props.icono}></i>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ContentRowMovies;
